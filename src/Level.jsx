5eac9f60610956a664c2780d79885b4058314f20
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2025-05-06 02:59:44 +0900

    style: white color when hover over levels

--- src/Level.jsx
+++ src/Level.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 export default function Level(props) {
     return (
-        <div onClick={() => props.onclick(props.index)} className={`transition-colors duration-300 ease-in-out cursor-pointer text-2xl ${props.index === props.level ? 'text-battery-charged-blue font-semibold' : 'text-steadfast'}`}>{props.index.toUpperCase()}</div>
+        <div onClick={() => props.onclick(props.index)} className={`transition-colors duration-300 ease-in-out cursor-pointer text-2xl hover:text-white ${props.index === props.level ? 'text-battery-charged-blue font-semibold' : 'text-steadfast'}`}>{props.index.toUpperCase()}</div>
     )
 }
