6c1048064c630cb5c693fc5a4db05ea34bd49081
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2025-05-16 04:20:31 +0900

    feat: implement arrow navigation and toggle for arrow visibility in App component

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
 	const [play, setPlay] = useState(true);
 	const [speed, setSpeed] = useState(4);
 	const [showMenu, setShowMenu] = useState(true);
+	const [showArrows, setShowArrows] = useState(true);
 	const [showReading, setShowReading] = useState(true);
 	const [showSenses, setShowSenses] = useState(false);
 	const [tango, setTango] = useState({
@@ -20,18 +21,9 @@ function App() {
 		senses: [["language"]]
 	});
 
-	const handlePlay = () => {
-		setPlay(prevPlay => !prevPlay);
-	};
 
-	useEffect(() => {
-		const handleSpace = e => {
-			if (e.code === 'Space') handlePlay();
-		};
-		window.addEventListener('keydown', handleSpace);
-		return () => {
-			window.removeEventListener('keydown', handleSpace);
-		};
+	const handlePlay = useCallback(() => {
+		setPlay(prevPlay => !prevPlay);
 	}, []);
 
 	useEffect(() => {
@@ -47,12 +39,27 @@ function App() {
 		fetchData();
 	}, [level]);
 
+	const [timeline, setTimeline] = useState([Math.floor(Math.random() * 100)]);
+	const [position, setPosition] = useState(0);
+
+	useEffect(() => {
+		setTimeline([Math.floor(Math.random() * 100)]);
+		setPosition(0);
+	}, [data]);
 
 	const getWord = useCallback(() => {
 		const len = data.length;
-		setTango(data[Math.floor(Math.random() * len)]);
-	}, [data]);
-
+		const index = Math.floor(Math.random() * len);
+		setTimeline(prevTimeline => {
+			const newTimeline = [...prevTimeline, index];
+			return newTimeline;
+		});
+		setPosition(prevPosition => {
+			const newPosition = prevPosition + 1;
+			setTango(data[timeline[newPosition] || index]);
+			return newPosition;
+		});
+	}, [data, timeline]);
 
 	useEffect(() => {
 		let interval;
@@ -83,6 +90,54 @@ function App() {
 		setSpeed(value);
 	};
 
+	const handleLeft = useCallback(() => {
+		if (position > 0) {
+			setPosition(prevPosition => {
+				const newPosition = prevPosition - 1;
+				setTango(data[timeline[newPosition]]);
+				return newPosition;
+			});
+		}
+	}, [data, position, timeline]);
+
+	useEffect(() => {
+		const arrowLeft = document.getElementById('arrow-left');
+		const arrowRight = document.getElementById('arrow-right');
+
+		const handleKeys = e => {
+			switch (e.code) {
+				case 'Space':
+					handlePlay();
+					break;
+				case 'ArrowLeft':
+				case 'KeyA':
+					arrowLeft.classList.add('active-fill');
+					handleLeft();
+					break;
+				case 'ArrowRight':
+				case 'KeyD':
+					arrowRight.classList.add('active-fill');
+					getWord();
+					break;
+				default:
+					break;
+			}
+		};
+
+		const handleKeyUp = () => {
+			[arrowLeft, arrowRight].forEach(arrow => arrow.classList.remove('active-fill'));
+		};
+
+		window.addEventListener('keydown', handleKeys);
+		window.addEventListener('keyup', handleKeyUp);
+		return () => {
+			window.removeEventListener('keydown', handleKeys);
+			window.removeEventListener('keyup', handleKeyUp);
+		};
+	}, [handlePlay, handleLeft, getWord]);
+
+	console.log(timeline.length);
+
 	return (
 		<>
 			<div className="absolute right-6 top-6 select-none z-10">
@@ -90,7 +145,7 @@ function App() {
 					<img src="./menu.svg" alt="hamburger" width="30px" height="30px" />
 				</div>
 				{showMenu &&
-					<div className="flex flex-col gap-4 p-4 mb-6 bg-river-styx rounded-xl text-center">
+					<div className="flex flex-col gap-4 p-4 mb-4 bg-river-styx rounded-xl text-center">
 						{[...Array(5)].map((_, i) => `n${i + 1}`).map(lvl => {
 							return <Level key={lvl} level={level} index={lvl} onclick={handleSelect} />;
 						})}
@@ -99,42 +154,68 @@ function App() {
 							<img src={play ? Pause : Play} alt="Pause / Play icon" className="mx-auto" />
 						</div>
 						<Slider min={3} max={10} step={1} value={speed} onChange={handleSlide} />
+						<div className="bg-coarse-wool rounded-md p-1 hover:cursor-pointer" onClick={() => setShowArrows(prevShowArrows => !prevShowArrows)}>
+							<svg className={`${showArrows ? "fill-battery-charged-blue" : "fill-steadfast"} transition-colors duration-300 ease-in-out hover:fill-white`} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#4b5975" viewBox="0 0 256 256">
+								<path
+									d="M104,128a8,8,0,0,1-8,8H56v24a8,8,0,0,1-13.66,5.66l-32-32a8,8,0,0,1,0-11.32l32-32A8,8,0,0,1,56,96v24H96A8,8,0,0,1,104,128Zm141.66-5.66-32-32A8,8,0,0,0,200,96v24H160a8,8,0,0,0,0,16h40v24a8,8,0,0,0,13.66,5.66l32-32A8,8,0,0,0,245.66,122.34ZM128,32a8,8,0,0,0-8,8V216a8,8,0,0,0,16,0V40A8,8,0,0,0,128,32Z"></path>
+							</svg>
+						</div>
 						<hr className="border-coarse-wool border-4" />
 						<Option show={showReading} onClick={() => setShowReading(prevReading => !prevReading)} value="読" />
 						<Option show={showSenses} onClick={() => setShowSenses(prevSenses => !prevSenses)} value="意" />
 					</div>
 				}
 			</div>
-			<div className={`transition-opacity duration-1000 ${show ? 'opacity-100' : 'opacity-0'}`}>
-				{!data?.length === 0 && <h1>Loading</h1>}
-				{showReading &&
-					<div className="font-util font-normal text-center text-4xl md:text-7xl md:tracking-[0.5em] text-steadfast">
-						{tango.reading}
-					</div>}
-				<div style={{
-					fontSize: window.innerWidth >= 768 ?
-						`${25 - (tango.word.length * 2.5)}rem` :
-						`${12 - (tango.word.length * 2.5)}rem`
-				}} className="text-8xl font-display font-black text-center text-only-olive mb-8">
-					{tango.word}
-				</div>
-				{showSenses &&
-					<>
-						<hr className="mb-8 border-steadfast" />
-						<div className="flex flex-col md:flex-row gap-4 md:gap-16 items-center text-left">
-							<a className="px-8 py-4 bg-river-styx rounded-lg hover:text-battery-charged-blue hover:font-semibold transition duration-300 ease-in-out" role="button"
-								href={`https://jisho.org/search/${tango.word}`} target="_blank">Open
-								Jisho.org</a>
-							<div className="font-mono text-only-olive ml-8 md:ml-0">
-								<ol className="list-decimal marker:text-battery-charged-blue ">
-									{data && tango.senses?.slice(0, 3).map((sense, idx) => <li key={idx}>{sense.slice(0, 3).join('; ')}</li>)}
-								</ol>
+			{!tango ? <h1>Loading...</h1> :
+				<>
+					<div className={`transition-opacity duration-1000 ${show ? 'opacity-100' : 'opacity-0'}`}>
+						{showReading &&
+							<div className="font-util font-normal text-center text-4xl md:text-7xl md:tracking-[0.5em] text-steadfast">
+								{tango.reading}
+							</div>}
+						<div style={{
+							fontSize: window.innerWidth >= 768 ?
+								`${25 - (tango.word.length * 2.5)}rem` :
+								`${12 - (tango.word.length * 2.5)}rem`
+						}} className="text-8xl font-display font-black text-center text-only-olive mb-8">
+							{tango.word}
+						</div>
+						{showSenses &&
+							<>
+								<hr className="mb-8 border-steadfast" />
+								<div className="flex flex-col md:flex-row gap-4 md:gap-16 items-center text-left">
+									<a className="px-8 py-4 bg-river-styx rounded-lg hover:text-battery-charged-blue hover:font-semibold transition duration-300 ease-in-out" role="button"
+										href={`https://jisho.org/search/${tango.word}`} target="_blank">Open
+										Jisho.org</a>
+									<div className="font-mono text-only-olive ml-8 md:ml-0">
+										<ol className="list-decimal marker:text-battery-charged-blue ">
+											{data && tango.senses?.slice(0, 3).map((sense, idx) => <li key={idx}>{sense.slice(0, 3).join('; ')}</li>)}
+										</ol>
+									</div>
+								</div>
+							</>
+						}
+					</div>
+					{
+						showArrows &&
+						<div className="mt-16 flex justify-center gap-8 cursor-pointer">
+							<div className="bg-river-styx rounded-lg p-3" onClick={handleLeft}>
+								<svg id="arrow-left" className="hover:fill-battery-charged-blue transition-colors duration-300 ease-in-out" xmlns="http://www.w3.org/2000/svg" width="44" height="44" fill="#4b5975" viewBox="0 0 256 256">
+									<path
+										d="M200,48V208a8,8,0,0,1-13.66,5.66l-80-80a8,8,0,0,1,0-11.32l80-80A8,8,0,0,1,200,48ZM72,40a8,8,0,0,0-8,8V208a8,8,0,0,0,16,0V48A8,8,0,0,0,72,40Z"></path>
+								</svg>
+							</div>
+							<div id="arrow-right" className="bg-river-styx rounded-lg p-3" onClick={getWord}>
+								<svg className="hover:fill-battery-charged-blue transition-colors duration-300 ease-in-out" xmlns="http://www.w3.org/2000/svg" width="44" height="44" fill="#4b5975" viewBox="0 0 256 256">
+									<path
+										d="M149.66,122.34a8,8,0,0,1,0,11.32l-80,80A8,8,0,0,1,56,208V48a8,8,0,0,1,13.66-5.66ZM184,40a8,8,0,0,0-8,8V208a8,8,0,0,0,16,0V48A8,8,0,0,0,184,40Z"></path>
+								</svg>
 							</div>
 						</div>
-					</>
-				}
+					}
+				</>
+			}
 
-			</div>
 		</>
 	);
 }
